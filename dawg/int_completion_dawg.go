757a7d0e8f84db0ea6a33c46d67bb82691cd7de0
ReanGD <reangd@yandex.ru>
2016-08-09 16:13:56 +0300

    port IntCompletionDAWG

---
+++ dawg/int_completion_dawg.go
@@ -0,0 +1,64 @@
+package dawg
+
+// status ok
+
+// StringUint32 ...
+type StringUint32 struct {
+	Key   string
+	Value uint32
+}
+
+// IntCompletionDAWG - Dict-like class based on DAWG.
+// It can store integer values for unicode keys and support key completion.
+type IntCompletionDAWG struct {
+	*DAWG
+	*CompletionDAWG
+}
+
+// Get - Return value for the given key.
+func (d *IntCompletionDAWG) Get(key string) (uint32, bool) {
+	return d.dct.Find([]byte(key))
+}
+
+// Items ...
+func (d *IntCompletionDAWG) Items(prefix string) []StringUint32 {
+	res := []StringUint32{}
+	bPrefix := []byte(prefix)
+	index := constRoot
+	var ok bool
+	if len(prefix) != 0 {
+		index, ok = d.dct.followBytes(bPrefix, index)
+		if !ok {
+			return res
+		}
+	}
+
+	completer := NewCompleter(d.dct, d.guide)
+	completer.start(index, bPrefix)
+
+	for completer.next() {
+		res = append(res, StringUint32{string(completer.key), completer.value()})
+	}
+
+	return res
+}
+
+// Load ...
+func (d *IntCompletionDAWG) Load(path string) error {
+	err := d.CompletionDAWG.Load(path)
+	if err != nil {
+		return err
+	}
+	d.DAWG.dct = d.CompletionDAWG.dct
+
+	return nil
+}
+
+// NewIntCompletionDAWG - constructor for IntCompletionDAWG
+func NewIntCompletionDAWG() *IntCompletionDAWG {
+	dawg := &IntCompletionDAWG{DAWG: NewDAWG(), CompletionDAWG: NewCompletionDAWG()}
+	dawg.DAWG.vDAWG = dawg
+	dawg.CompletionDAWG.vDAWG = dawg
+
+	return dawg
+}
